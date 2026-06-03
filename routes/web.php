5eb24b016bb7b18067e43452a8a427ed7de1a920
Kaka <kakagastya2@gmail.com>
2026-06-03 12:26:31 +0700

    Pertemuan 13

--- routes/web.php
+++ routes/web.php
@@ -46,6 +46,12 @@ Route::get('/pertemuan6', function () {
     return view('pertemuan6');
 });
 
+Route::get('/pegawai/tambah', [PegawaiDBController::class, 'tambah']);
+Route::post('/pegawai/store', [PegawaiDBController::class, 'store']);
+Route::get('/pegawai/edit/{id}', [PegawaiDBController::class, 'edit']);
+Route::post('/pegawai/update', [PegawaiDBController::class, 'update']);
+Route::get('/pegawai/hapus/{id}', [PegawaiDBController::class, 'hapus']);
+Route::get('/pegawai/cari', [PegawaiDBController::class, 'cari']);
 // Route::get('/pegawai','PegawaiDBController::class,@index');
 
 Route::get('/pegawai', [PegawaiDBController::class, 'index']);
